491c8b8ea3fbaf81281f3337a9af534ecae9d276
Mehran Mohammadi <mohammadi000098@gmail.com>
2022-09-05 08:06:51 +0430

    change list style

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,18 @@ const storeCounter = useListStore();
 </script>
 
 <template>
-  <div class=" dark:bg-slate-800 justify-center items-center flex flex=wrap flex-col ">
+  <div class=" w-screen h-screen dark:bg-slate-800 justify-center items-center flex flex=wrap flex-col ">
   <h1 >Mehran`s ToDoList</h1>
   <input type="text">
   <input class="bg-slate-900" type="number">
 
   <button>add</button>
-
-  <p class="ListOfToDo w-80  bg-lime-300 m-8 p-8 rounded-md flex flex-col "  v-for="(List,index) in storeCounter.List" :key="index">
+<div class="bg-slate-900 w-9/12 p-2 justify-center items-center flex flex-wrap">
+  <p class="w-80  bg-blue-300 m-3 p-4 rounded-md w-9/12 "  v-for="(List,index) in storeCounter.List" :key="index">
     {{ List.name }}
-    <span class=" bg-red-500 w-10 justify-center rounded-full  ">x</span>
+    <span class=" bg-red-600 cursor-pointer text-zinc-50 w-10 justify-center rounded-full font-bold float-right flex justify-center items-center">x</span>
   </p>
+</div>
   </div>
 </template>
 
