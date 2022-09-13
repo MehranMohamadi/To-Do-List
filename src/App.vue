19f1758d2533a265828498d7b5a754b99034bd44
Mehran Mohammadi <mohammadi000098@gmail.com>
2022-09-13 17:11:42 +0430

    addItem

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,25 @@ const storeCounter = useListStore();
 const newTodo = ref('');
 const deadline = ref('');
 const taskDone = ref(true)
-
-function addItem() {
+const isDone = ref(true)
+const newStyle=ref('')
+async function addItem() {
+  await storeCounter.List.push({name: newTodo.value, deadline: deadline.value})
   newTodo.value = ''
+  console.log(newTodo.value)
   deadline.value = 0
-  storeCounter.List.push({name: newTodo, deadline: deadline})
+
   storeCounter.taskDone(taskDone)
   console.log(taskDone)
 }
-function deleteItem(index){
-  storeCounter.List.splice(index,1)
 
+function deleteItem(index) {
+  storeCounter.List.splice(index, 1)
+
+}
+
+function done(index) {
+  newStyle.value='bg-green-300'
 }
 
 
@@ -26,19 +34,26 @@ function deleteItem(index){
 <template>
   <div class=" w-screen h-screen dark:bg-slate-800 justify-center items-center flex flex=wrap flex-col ">
     <h1 class="text-7xl">Mehran`s ToDoList</h1>
-    <input v-model="newTodo"
+    <input v-model.lazy="newTodo"
            class="bg-blue-300 border-none  w-1/3 m-4 p-4 selection:bg-fuchsia-300 selection:text-fuchsia-900 "
            type="text"
            placeholder="">
+    {{ storeCounter.List }}
     <input v-model="deadline" class="bg-blue-900" type="text" placeholder="example:3 days">
 
-    <button class="bg-blue-500 rounded-full font-bold text-2xl text-white m-3 p-4 " @click="addItem">+</button>
-    <div class="bg-slate-900 xl:w-1/2  w-9/12 p-2 justify-center items-center flex flex-wrap flex-col">
-      <p class="hover:rounded-full selection:bg-fuchsia-300 selection:text-fuchsia-900  w-80 italic cursor-pointer hover:not-italic font-bold  bg-blue-300 m-3 p-4 rounded-md w-9/12 "
+    <button class="bg-blue-500 rounded-full font-bold text-2xl
+     text-white m-3 p-4 " @click.prevent="addItem">
+      +
+    </button>
+    <div class="bg-slate-900 xl:w-1/2  w-9/12 p-2 justify-center items-center
+     flex flex-wrap flex-col">
+      <p @click="done(index)" :class="newStyle"
+         class="hover:bg-green-300 selection:bg-fuchsia-300 selection:text-fuchsia-900  w-80 italic cursor-pointer hover:not-italic font-bold  bg-blue-300 m-3 p-4 rounded-md w-9/12 "
          v-for="(List,index) in storeCounter.List" :key="index">
         {{ List.name }}
         <span
-            @click="()=>deleteItem(index)" class=" hover:bg-zinc-50 hover:text-red-600 bg-red-600 cursor-pointer text-zinc-50 w-10 justify-center rounded-full font-bold float-right flex justify-center items-center">x</span>
+            @click="()=>deleteItem(index)"
+            class=" hover:bg-zinc-50 hover:text-red-600 bg-red-600 cursor-pointer text-zinc-50 w-10 justify-center rounded-full font-bold float-right flex justify-center items-center">x</span>
       </p>
 
     </div>
--- src/stores/List.js
+++ src/stores/List.js
@@ -3,17 +3,15 @@ import {defineStore} from 'pinia'
 export const useListStore = defineStore('counter', {
     state: () => {
         return {
-            List: [{name: 'ali', deadline: '2', isDone: false, id: 0}, {
-                name: 'omid',
-                deadline: '5'
-            },]
+            List: [{name: 'ali', deadline: '2', id: 0},
+                {name: 'omid', deadline: '5', id: 1},]
         }
     },
     // could also be defined as
     // state: () => ({ count: 0 })
     actions: {
         taskDone(taskDone) {
-           return  taskDone = !taskDone
+            return taskDone = !taskDone
         },
     },
 })
