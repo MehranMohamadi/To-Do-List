500de0c08d5c98de3ac585c3e2c61c8919acfb8d
Mehran Mohammadi <mohammadi000098@gmail.com>
2022-09-25 11:58:42 +0330

    use pinia

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,12 @@ import {ref} from "vue";
 
 
 const storeCounter = useListStore();
-const newTodo = ref('');
-const id = ref(0)
 
-const newStyle = ref('')
 
-function addItem() {
-  console.log(newTodo.value)
-  newTodo.value !== '' ? storeCounter.List.push({name: newTodo.value, id: id.value++}) : alert('null input');
-  newTodo.value=''
-}
 
-function deleteItem(index) {
-  storeCounter.List.splice(index, 1);
 
-}
 
-function done() {
-  newStyle.value = 'bg-green-300'
-}
+
 
 
 </script>
@@ -31,7 +18,7 @@ function done() {
   <div class=" w-screen h-screen  justify-center items-center flex flex=wrap flex-col ">
 
     <div class="flex flex-wrap flex-row  bg-slate-900 w-1/3 rounded-t-lg ">
-      <input v-model.lazy="newTodo"
+      <input v-model.lazy="storeCounter.newTodo"
              class="rounded-md bg-slate-700 border-none
               w-2/3 m-2 p-2 selection:bg-fuchsia-300 selection:text-fuchsia-900 "
              type="text"
@@ -39,20 +26,20 @@ function done() {
 
       <button class="bg-blue-400 rounded-full font-bold text-2xl
      text-white m-2 p-2 w-1/6 hover:bg-blue-600 float-right   "
-              @click.prevent="addItem">
+              @click.prevent="storeCounter.addItem">
         +
       </button>
 
     </div>
     <div class=" bg-slate-900  xl:w-1/2 rounded-md  w-9/12 p-2 justify-center items-center
      flex  flex-col">
-      <p @click="done(index)" :class="newStyle"
+      <p @click="storeCounter.done()" :class="newStyle"
          class="hover:bg-green-300 break-words selection:bg-fuchsia-300 selection:text-fuchsia-900
           w-80 italic cursor-pointer hover:not-italic font-bold  bg-blue-300 m-3 p-4 rounded-md w-5/6 "
          v-for="(List,index) in storeCounter.List" :key="index">
         {{ List.name }}
         <span
-            @click="()=>deleteItem(index)"
+            @click="()=>storeCounter.deleteItem(index)"
             class=" hover:bg-zinc-50 hover:text-red-600 bg-red-600 cursor-pointer text-zinc-50 w-10
             justify-center rounded-full font-bold float-right flex justify-center items-center">x</span>
       </p>
--- src/stores/List.js
+++ src/stores/List.js
@@ -1,16 +1,36 @@
 import {defineStore} from 'pinia'
+import {ref} from "vue";
 
 export const useListStore = defineStore('counter', {
     state: () => {
         return {
-            List: []
+            List: [],
+            newTodo: '',
+            id: 0,
+            newStyle: ''
         }
     },
     // could also be defined as
     // state: () => ({ count: 0 })
     actions: {
-        taskDone(taskDone) {
-            return taskDone = !taskDone
+
+        deleteItem(index) {
+            this.List.splice(index, 1);
+            console.log('deleteItem')
+
         },
+        addItem() {
+            console.log(this.newTodo)
+            this.newTodo.value !== '' ?
+                this.List.push({name: this.newTodo, id: this.id++})
+                : alert('null input');
+            this.newTodo = ''
+        },
+
+
+        done() {
+            this.newStyle = 'bg-green-300'
+        }
+
     },
 })
